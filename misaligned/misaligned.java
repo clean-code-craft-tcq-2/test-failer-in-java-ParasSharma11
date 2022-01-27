a6eaa52ea2ec4e688085e8afc66891169c4dd12e
ParasSharma11 <90769825+ParasSharma11@users.noreply.github.com>
2022-01-27 09:49:00 +0530

    Update misaligned.java

--- misaligned/misaligned.java
+++ misaligned/misaligned.java
@@ -1,18 +1,20 @@
-public class Misaligned {
+public class misaligned {
     static int printColorMap() {
         String majorColors[] = {"White", "Red", "Black", "Yellow", "Violet"};
         String minorColors[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+        Map<int, String> colorPairWithIndex = new HashMap<int, String>();
         int i = 0, j = 0;
         for(i = 0; i < 5; i++) {
             for(j = 0; j < 5; j++) {
+                colorPairWithIndex.put( (i +j) , majorColors[i] + " " + minorColors[i]);
                 System.out.printf("%d | %s | %s\n", i * 5 + j, majorColors[i], minorColors[i]);
             }
         }
-        return i * j;
+        return colorPairWithIndex;
     }
     public static void main(String[] args) { 
-        int result = printColorMap();
-        assert(result == 25);
+        Map<int, String result = printColorMap();
+	    assert(result.get(1).equals("White Blue"));
         System.out.println("All is well (maybe!)");
     }
 }
