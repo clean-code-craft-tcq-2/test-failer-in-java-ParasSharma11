8fa1876a0a070aad21059f4ed21e0eb7869a0124
ParasSharma11 <90769825+ParasSharma11@users.noreply.github.com>
2022-01-28 18:57:37 +0530

    Update misaligned.java

--- misaligned/misaligned.java
+++ misaligned/misaligned.java
@@ -1,22 +1,33 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.*;
 public class misaligned {
-    static Map<Integer, String> printColorMap() {
-        String majorColors[] = {"White", "Red", "Black", "Yellow", "Violet"};
-        String minorColors[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-        Map<Integer, String> colorPairWithIndex = new HashMap<Integer, String>();
+    static String majorColors[] = {"White", "Red", "Black", "Yellow", "Violet"};
+    static String minorColors[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+    static String colorPairEntry(int majorColorIndex , int minorColorIndex ){
+        String index = String.valueOf(majorColorIndex*5 + minorColorIndex +1);
+        String str = new String(index+" | "+majorColors[majorColorIndex]+" | "+ minorColors[minorColorIndex]);
+        print(str);
+        return str;
+    }
+    
+    static void  print(String colorPairEntryWithIndex){
+        System.out.println(colorPairEntryWithIndex);
+    }
+    static String[] printColorMap() {
+        String[] colorPairWithIndex = new String[25];
         int i = 0, j = 0;
         for(i = 0; i < 5; i++) {
             for(j = 0; j < 5; j++) {
-                colorPairWithIndex.put( (i +j) , majorColors[i] + " " + minorColors[i]);
-                System.out.printf("%d | %s | %s\n", i * 5 + j, majorColors[i], minorColors[i]);
+                colorPairWithIndex[i*5 +j]= colorPairEntry(i,j);
             }
         }
         return colorPairWithIndex;
     }
     public static void main(String[] args) { 
-        Map<Integer, String> result = printColorMap();
-	    assert(result.get(1).equals("White Blue"));
+        String[] result = printColorMap();
+	    assert(result.length == 25);
+	    assert(result[0].equals("1 | White | Blue"));
         System.out.println("All is well (maybe!)");
     }
 }
