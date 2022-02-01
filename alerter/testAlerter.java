1acfc059fa780b8f292ddcf8eb005422e1aae819
ParasSharma11 <90769825+ParasSharma11@users.noreply.github.com>
2022-02-01 16:12:07 +0530

    Update testAlerter.java

--- alerter/testAlerter.java
+++ alerter/testAlerter.java
@@ -1,1 +1,13 @@
+package alerter;
 
+public class testAlerter{
+  static void testAlertInCelcius(){
+    
+    alerter.alertInCelcius(400.5);
+    alerter.alertInCelcius(303.6);
+    alerter.alertInCelcius(522.6);
+    assert(alertFailureCount == 2);
+    printf("%d alerts failed.\n", alertFailureCount);
+    printf("All is well (maybe!)\n");
+  }
+}
